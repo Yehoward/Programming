92ebdb318358672238720552d969a4cabcf958db
yehoslavrudenco@gmail.com <yehoslavrudenco@gmail.com>
2022-02-19 19:58:31 +0200

    Brought the code up to the standards

--- randomTextGenerator.py
+++ randomTextGenerator.py
@@ -1,11 +1,16 @@
 from random import randint, choice
 
-allowed = "+-()/*1234567890"
-expresi = ""
-for _ in range(100000):
-    for _ in range(50):
-        expresi+=choice(allowed)
-    expresi+='\n'
+def main():
+    allowed = "+-()/*1234567890"
+    expresi = ""
+    for _ in range(100000):
+        for _ in range(50):
+            expresi+=choice(allowed)
+        expresi+='\n'
 
-with open("formule.txt", "w") as file:
-    file.write(expresi)
+    with open("formule.txt", "w") as file:
+        file.write(expresi)
+
+
+if __name__=="__main__":
+    main()
